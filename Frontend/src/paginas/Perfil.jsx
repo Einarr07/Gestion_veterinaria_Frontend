510753c6dc227d212fd7185e04a99be767c8e10a
Einarr07 <aidenperse@gmail.com>
2023-09-03 18:36:57 -0500

    Frontend terminado

--- Frontend/src/paginas/Perfil.jsx
+++ Frontend/src/paginas/Perfil.jsx
@@ -10,7 +10,7 @@ const Perfil = () => {
             <div>
                 <h1 className='font-black text-4xl text-gray-500'>Perfil</h1>
                 <hr className='my-4' />
-                <p className='mb-8'>Este módulo te permite visualizar el perfil del usuario......</p>
+                <p className='mb-8'>Aquí puedes actualizar tu perfil</p>
             </div>
 
             <div className='flex justify-around gap-x-8 flex-wrap gap-y-8 md:flex-nowrap'>
